3492d994600008f3fddf0211d105613e497fb708
Gianna Ysabel Capino Besmonte <gbesmont@usc.edu>
2025-06-16 12:20:24 -0700

    more server edits

--- api/server.js
+++ api/server.js
@@ -1,8 +1,29 @@
 const express = require('express');
-const server = express();
+const cors = require('cors');
+const Pet = require('./pet-model');
 
+const server = express();
+server.use(cors());
+
+server.get('/api/pets', async (req, res, next) => {
+    try {
+        const pets = await Pet.find();
+        res.json(pets); //defaults to 200 ok
+    }
+    catch(err){
+        next(err);
+    }
+});
+
+//put catch all at the end- order matters
 server.use('/*', (req, res, next) => {
-    res.status(200).json({message: 'Hello World!'});
+    next({message: 'not found!', status: 404})
 })
+
+server.use((err, req, res, next) => {
+    const {message, status = 500}  = err;
+    console.log(message);
+    res.status(status).json({message});
+});
 
 module.exports = server;
